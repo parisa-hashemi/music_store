275ed2bc979ce1ceb791ee8a976265c3303260c3
Parisa <phashemi775@gmail.com>
2026-07-11 13:32:55 +0330

    fix bug

--- API/API/settings.py
+++ API/API/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://music-store-snowy.vercel.app",
     "https://*.vercel.app",  # Vercel
     "https://*.onrender.com",  # Render
 ]
